f98eb8ed8015b7581698d25aab6024c4014243c4
Rai <rai@rbutera.com>
2018-02-07 09:42:08 +0000

    Revert "removed duplicates"
    This reverts commit 827bafc17ddd00a701584d0f894e74026424b432.

--- src/character_frequency.h
+++ src/character_frequency.h
@@ -11,6 +11,9 @@ typedef struct charfreq_table
     int *frequency;  // an array of frequencies, corresponds to characters
 } charfreq_table;
 
+// creates a charfreq_table
+charfreq_table *charfreq_table_create();
+
 /* 
 processes all of input, creating a character frequency table
 using countCharacters to update arr and freq
